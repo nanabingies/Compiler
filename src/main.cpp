ccf4dd3cd6fbbaee3d0361ed38a36b8cc672f031
Nana Bingies <nanabingies1@gmail.com>
2025-06-11 20:16:39 +0000

    update main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include "../include/lexer.hpp"
+#include "../include/utils.hpp"
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "%s: error: no input files\n", program_name.c_str());
+        return EXIT_FAILURE;
+    }
+
+    if (file_exists(argv[1]) == false) {
+        std::fprintf(stderr, "%s: no such file or directory: %s\n", program_name.c_str(), argv[1]);
+        std::fprintf(stderr, "%s: error: no input files\n", program_name.c_str());
+        return EXIT_FAILURE;
+    }
+
+    input_stream = std::make_unique<std::fstream>();
+    input_stream->open(argv[1], std::fstream::in);
+
+
+
+    input_stream->close();
+
     return 0;
 }
