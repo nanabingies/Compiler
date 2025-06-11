8685aebb1981e0212e5e48b0075e386d16786026
Nana Bingies <nanabingies1@gmail.com>
2025-06-11 20:16:22 +0000

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,4 +1,20 @@
-#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 
+// compile name
+static std::string program_name = "compiler";
 
+// input file stream
+static std::unique_ptr<std::fstream> input_stream{};
+
+// Current Line number
+static int curr_line = 1;
+
+inline bool file_exists(const std::string& name) {
+    std::ifstream f(name.c_str());
+    return f.good();
+}
