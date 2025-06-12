bb6821e5d794a650b74d85e28f97b28de173f764
Nana Bingies <nanabingies1@gmail.com>
2025-06-12 14:23:29 +0000

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,58 +5,88 @@
 namespace parser {
     void parse_statements() {
         while (true) {
-            auto tok = lexer::getNextToken();
+            int32_t tok = lexer::getNextToken();
             
             switch (tok) {
                 case tok_invalid: {
-                    std::fprintf(stderr, "Invalid token passed. Line %d\n", static_cast<int>(curr_line));
+                    std::fprintf(stderr, "Invalid token passed. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     return;
                 }
 
-                case tok_eof:
+                case tok_eof: {
+                    std::fprintf(stderr, "EOF encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     return;
+                }
 
-                case tok_plus:
+                case tok_plus: {
+                    std::fprintf(stderr, "'+' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_minus:
+                case tok_minus: {
+                    std::fprintf(stderr, "'-' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_mul:
+                case tok_mul: {
+                    std::fprintf(stderr, "'*' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_div:
+                case tok_div: {
+                    std::fprintf(stderr, "'/' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_cmplt:
+                case tok_cmplt: {
+                    std::fprintf(stderr, "'<' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_cmpgt:
+                case tok_cmpgt: {
+                    std::fprintf(stderr, "'>' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_equals:
+                case tok_equals: {
+                    std::fprintf(stderr, "'=' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_open_b:
+                case tok_open_b: {
+                    std::fprintf(stderr, "'(' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_close_b:
+                case tok_close_b: {
+                    std::fprintf(stderr, "')' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_open_c:
+                case tok_open_c: {
+                    std::fprintf(stderr, "'{' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_close_c:
+                case tok_close_c: {
+                    std::fprintf(stderr, "'}' encountered. Token '%c', Line %d\n", static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_identifier:
+                case tok_identifier: {
+                    std::fprintf(stderr, "Identifier '%s', Token '%c', Line %d\n", identifier_str.c_str(), static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_number:
+                case tok_number: {
+                    std::fprintf(stderr, "Number '%d', Token '%c', Line %d\n", static_cast<int>(token_number_int), static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
 
-                case tok_keyword:
+                case tok_keyword: {
+                    std::fprintf(stderr, "Keyword '%s', Token '%c', Line %d\n", keyword_str.c_str(), static_cast<char>(tok), static_cast<int>(curr_line));
                     break;
+                }
     
                 default: {
                     std::fprintf(stderr, "Invalid token passed. Line %d\n", static_cast<int>(curr_line));
