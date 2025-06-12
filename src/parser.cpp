4172219ada5d1fcd12b20bc257fa472c9f17e15b
Nana Bingies <nanabingies1@gmail.com>
2025-06-12 22:32:09 +0000

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,12 +1,38 @@
 #include "../include/lexer.hpp"
 #include "../include/parser.hpp"
+
+#include "../include/ast.hpp"
 #include "../include/utils.hpp"
 
 namespace parser {
-    void parseStatements() {
+    void parseBuffer() {
         curr_token_index = 0;
         while (true) {
-            int32_t tok = lexer::getNextToken();
+            auto tok = lexer::getNextToken();
+
+            switch (tok) {
+                case tok_identifier:
+                    std::fprintf(stdout, "Handling tok_identifier\n");
+                    parseStatement();
+                    break;
+                case tok_keyword:
+                    std::fprintf(stdout, "Handling tok_keyword\n");
+                    parseKeywordStatement();
+                    break;
+                case tok_print:
+                    std::fprintf(stderr, "Print Not yet implemented\n");
+                    break;
+                default:
+                    std::fprintf(stderr, "Unknown token %c\n", static_cast<char>(tok));
+                    std::exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    void parseTokenStatement(const int tok) {
+        curr_token_index = 0;
+        while (true) {
+            //int32_t tok = lexer::getNextToken();
             
             switch (tok) {
                 case tok_invalid: {
@@ -16,6 +42,7 @@ namespace parser {
 
                 case tok_eof: {
                     std::fprintf(stderr, "EOF encountered. Line %d\n", curr_line);
+                    curr_buffer = "";
                     return;
                 }
 
@@ -102,15 +129,53 @@ namespace parser {
         }
     }
 
+    void parseKeywordStatement() {
+        if (keyword_str.empty())    return;
+
+        if (keyword_str == "int") {
+            // read identifier token
+            lexer::getNextToken();
+            auto identStr = identifier_str;
+
+            if (lexer::getNextToken() != tok_equals) {
+                std::fprintf(stderr, "error: Expected '=' at line %d\n", curr_line);
+                std::exit(EXIT_FAILURE);
+            }
+
+            // is it a number, string or identifier
+            auto nextToken = lexer::getNextToken();
+            if (nextToken == tok_number) {
+                auto variableAst = std::make_unique<ast::VariableExprAST<int>>(identStr, token_number_int);
+            } else if (nextToken == tok_double) {
+                auto variableAst = std::make_unique<ast::VariableExprAST<double>>(identStr, token_number_int);
+            } else if (nextToken == tok_identifier) {
+                auto variableAst = std::make_unique<ast::VariableExprAST<std::string>>(identStr, identifier_str);
+            }
+        }
+
+        else if (keyword_str == "char") {
+            // read next token
+            //auto nextToken = lexer::getNextToken();
+
+            if (lexer::getNextToken() != '=') {
+                std::fprintf(stderr, "error: Expected '=' at line %d\n", curr_line);
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    }
+
     void parseStatement() {
+        if (identifier_str.empty()) return;
+
+        if (identifier_str == "int")
+            return parseKeywordStatement();
+    }
+
+    void parseFunctionPrototype() {
 
     }
 
-    void parserFunctionPrototype() {
-
-    }
-
-    void parserFunction() {
+    void parseFunction() {
 
     }
 
