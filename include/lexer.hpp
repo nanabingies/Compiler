b960ea3b6200f09aa42a73a23f6c09629015138c
Nana Bingies <nanabingies1@gmail.com>
2025-06-12 00:59:55 +0000

    Update lexer.hpp

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -7,7 +7,8 @@
 // Lexer
 //===----------------------------------------------------------------------===//
 
-enum tokens : std::uint32_t {
+enum tokens : std::int32_t {
+    tok_invalid = -1,
     tok_eof = 0,
 
     // Binary operators
@@ -19,10 +20,29 @@ enum tokens : std::uint32_t {
     tok_cmpgt,  // >
     tok_equals, // =
 
+    // Unary operators
+    tok_open_b,     // (
+    tok_close_b,    // )
+    tok_open_c,   // {
+    tok_close_c,    // }
+
     // Language impls
     tok_identifier,
     tok_number,
     tok_keyword,
 };
+
+extern int curr_token;
+extern int curr_token_index;
+
+extern std::string identifier_str;
+extern std::string keyword_str;
+
+extern int token_number_int;
+extern std::string token_number_str;
+
+namespace lexer {
+    tokens getNextToken();
+}
 
 #endif
