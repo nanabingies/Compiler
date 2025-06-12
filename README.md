97fb02a2576166985d3969ad0607374d4e4253ae
Nana Bingies <nanabingies1@gmail.com>
2025-06-12 01:00:28 +0000

    update utils

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Compiler
 A simple compiler for the **C** programming language
 
-I've recently starting reading up on compiler theory and decided to write a compile for the **C** language. This project will be continuously updated till it turns to a full fledged program.
+I've recently starting reading up on compiler theory and decided to write a compile for the **C** language.
+The compiler currently supports opening and compiling just one file into the target architecture.
+This project will be continuously updated till it turns to a full fledged program.
--- include/utils.hpp
+++ include/utils.hpp
@@ -1,20 +1,31 @@
+#ifndef __UTILS_HPP_
+#define __UTILS_HPP_
+
+#include <cctype>
 #include <memory>
 #include <string>
 #include <vector>
 #include <cstdlib>
 #include <fstream>
+#include <sstream>
 #include <iostream>
 
 // compile name
 static std::string program_name = "compiler";
 
 // input file stream
-static std::unique_ptr<std::fstream> input_stream{};
+static std::fstream input_stream{};
 
 // Current Line number
-static int curr_line = 1;
+extern int curr_line;
 
+// String to hold current line number
+extern std::string curr_buffer;
+
+// Check if input file exists on filesystem
 inline bool file_exists(const std::string& name) {
     std::ifstream f(name.c_str());
     return f.good();
 }
+
+#endif
