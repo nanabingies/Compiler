cadc568a70396d644989ebd251557b1a5630720b
Nana Bingies <nanabingies1@gmail.com>
2025-06-13 15:12:09 +0000

    Update parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -3,27 +3,34 @@
 
 #include "ast.hpp"
 #include <map>
+#include <utility>
 
 extern std::map<char, int> binopPrecedence;
 
 namespace parser {
-    void parseBuffer();
-
-    std::unique_ptr<ast::ExprAST> parseStatement();
-
-    void parseFunctionPrototype();
-
-    void parseFunction();
-
-    std::unique_ptr<ast::ExprAST> parseKeywordStatement();
-
-    static std::unique_ptr<ast::ExprAST> ParseBinOpRHS(int, std::unique_ptr<ast::ExprAST>);
-
-    std::unique_ptr<ast::ExprAST> parseNumberExpr();
-
-    std::unique_ptr<ast::ExprAST> parsePrimary();
-
     int getOperatorPrecedence();
-}
+
+    void parseTokenStatement(int);
+
+    class Parser {
+        std::string filename;
+
+    public:
+        explicit Parser(std::string name) : filename(name) {}
+
+        std::unique_ptr<ast::ExprAST> parseStatement();
+        std::unique_ptr<ast::ExprAST> parseKeywordStatement();
+        std::unique_ptr<ast::ExprAST> parseFunction();
+        std::unique_ptr<ast::ExprAST> parseFunctionPrototype();
+
+        std::unique_ptr<ast::ExprAST> ParseBinOpRHS(int, std::unique_ptr<ast::ExprAST>);
+        std::unique_ptr<ast::ExprAST> parseNumberExpr();
+        std::unique_ptr<ast::ExprAST> parseDoubleExpr();
+
+        std::unique_ptr<ast::ExprAST> parsePrimary();
+    };
+
+    void parseBuffer(Parser);
+};
 
 #endif
