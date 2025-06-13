8c5b099f8d75205535c49eccd57c1205142989ee
Nana Bingies <nanabingies1@gmail.com>
2025-06-13 00:45:34 +0000

    Update parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,18 +1,29 @@
 #ifndef __PARSER_HPP
 #define __PARSER_HPP
 
-#include <iostream>
+#include "ast.hpp"
+#include <map>
+
+extern std::map<char, int> binopPrecedence;
 
 namespace parser {
     void parseBuffer();
 
-    void parseStatement();
+    std::unique_ptr<ast::ExprAST> parseStatement();
 
     void parseFunctionPrototype();
 
     void parseFunction();
 
-    void parseKeywordStatement();
+    std::unique_ptr<ast::ExprAST> parseKeywordStatement();
+
+    static std::unique_ptr<ast::ExprAST> ParseBinOpRHS(int, std::unique_ptr<ast::ExprAST>);
+
+    std::unique_ptr<ast::ExprAST> parseNumberExpr();
+
+    std::unique_ptr<ast::ExprAST> parsePrimary();
+
+    int getOperatorPrecedence();
 }
 
 #endif
