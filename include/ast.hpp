191942c14741b887207375f2b37bd24ce83b8f0b
Nana Bingies <nanabingies1@gmail.com>
2025-06-14 11:43:09 +0000

    Update ast.hpp

--- include/ast.hpp
+++ include/ast.hpp
@@ -2,27 +2,43 @@
 #define __AST_HPP
 
 #include "../include/lexer.hpp"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
 #include <string>
 #include <memory>
+#include <utility>
+
+extern std::unique_ptr<llvm::LLVMContext> TheContext;
+extern std::unique_ptr<llvm::Module> TheModule;
+extern std::unique_ptr<llvm::IRBuilder<>> Builder;
+extern std::map<std::string, llvm::Value*> NamedValues;
 
 namespace ast {
+
     class ExprAST {
     public:
         virtual ~ExprAST() = default;
+        //virtual llvm::Value* codegen() = 0;
     };
 
     class IntNumberExprAST : public ExprAST {
         int value;
 
     public:
-        IntNumberExprAST(const double value) : value(value) {}
+        explicit IntNumberExprAST(const double value) : value(value) {}
+
+        //llvm::Value* codegen() override;
     };
 
     class DoubleNumberExprAST : public ExprAST {
         double value;
 
     public:
-        DoubleNumberExprAST(const double value) : value(value) {}
+        explicit DoubleNumberExprAST(const double value) : value(value) {}
+
+        //llvm::Value* codegen() override;
     };
 
     template<typename T>
@@ -31,8 +47,10 @@ namespace ast {
         T value;
 
     public:
-        VariableExprAST(const std::string& name, T value)
-            : name(name), value(std::move(value)) {}
+        VariableExprAST(std::string  name, T value)
+            : name(std::move(name)), value(std::move(value)) {}
+
+        //llvm::Value* codegen() override;
     };
 
     class BinaryExprAST : public ExprAST {
@@ -43,6 +61,8 @@ namespace ast {
     public:
         BinaryExprAST(const tokens op, std::unique_ptr<ExprAST> left, std::unique_ptr<ExprAST> right)
             : op(op), LHS(std::move(left)), RHS(std::move(right)) {}
+
+        //llvm::Value* codegen() override;
     };
 
     class IdentifierExprAST : public ExprAST {
@@ -50,8 +70,10 @@ namespace ast {
         std::unique_ptr<ExprAST> value;
 
     public:
-        IdentifierExprAST(const std::string& name, std::unique_ptr<ExprAST> value)
-            : name(name), value(std::move(value)) {}
+        IdentifierExprAST(std::string  name, std::unique_ptr<ExprAST> value)
+            : name(std::move(name)), value(std::move(value)) {}
+
+        //llvm::Value* codegen() override;
     };
 };
 
