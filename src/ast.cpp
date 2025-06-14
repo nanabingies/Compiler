733041ff80a2d4a9059e87d0e102a9aacc83326b
Nana Bingies <nanabingies1@gmail.com>
2025-06-14 21:10:26 +0000

    Update ast.cpp

--- src/ast.cpp
+++ src/ast.cpp
@@ -5,6 +5,38 @@ namespace ast {
     std::unique_ptr<llvm::Module> llvmModule;
     std::unique_ptr<llvm::IRBuilder<>> llvmBuilder;
     std::map<std::string, llvm::Value*> NamedValues;
+    std::string defaultFunctionName;
+
+    VariableExprAST::VariableExprAST(std::string c_name, int c_value) {
+        name = std::move(c_name);
+        value = std::move(c_value);
+        //NamedValues[name] = llvm::ConstantInt(llvm::Type::getInt64Ty(*llvmContext), llvm::APInt(value));
+        llvm::Function* function = llvmModule->getFunction(defaultFunctionName);
+        if (function == nullptr) {
+            // Create function
+            llvm::FunctionType *funcType = llvm::FunctionType::get(llvm::Type::getVoidTy(*llvmContext), false);
+            function = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, defaultFunctionName, *llvmModule);
+        }
+
+        if (function == nullptr) {
+            std::cerr << "Failed to get function for variable definition" << std::endl;
+            return;
+        }
+
+        llvm::BasicBlock *BB = llvm::BasicBlock::Create(*llvmContext, "entry", function);
+        llvmBuilder->SetInsertPoint(BB);
+
+        llvm::Type *intType = llvm::Type::getInt32Ty(*llvmContext);
+        llvm::Value *xAlloca = llvmBuilder->CreateAlloca(intType, nullptr, name);
+        llvm::Value *intConstant = llvm::ConstantInt::get(intType, value);
+
+        llvmBuilder->CreateStore(intConstant, xAlloca);
+
+        // Add return void
+        llvmBuilder->CreateRetVoid();
+
+        NamedValues[name] = intConstant;
+    }
 
     llvm::Value* ast::IntNumberExprAST::codegen() {
         return llvm::ConstantInt::get(*llvmContext, llvm::APInt(8, value, true));
@@ -20,16 +52,23 @@ namespace ast {
     }
 
     llvm::Value* ast::BinaryExprAST::codegen() {
-        llvm::Value* Left = LHS->codegen();
-        llvm::Value* Right = RHS->codegen();
-        if (!Left || !Right)
+        llvm::Value* left = LHS->codegen();
+        llvm::Value* right = RHS->codegen();
+        if (!left || !right)
             return nullptr;
 
-        /*switch (op) {
-            case '+':
-                return llvmBuilder-
-        }*/
-       return nullptr;
+        switch (op) {
+            case tok_plus:
+                return llvmBuilder->CreateAdd(left, right, "c_add");
+            case tok_minus:
+                return llvmBuilder->CreateSub(left, right, "c_sub");
+            case tok_mul:
+                return llvmBuilder->CreateMul(left, right, "c_mul");
+            case tok_div:
+                return llvmBuilder->CreateSDiv(left, right, "c_div");
+            default:
+                return nullptr;
+        }
     }
 
     llvm::Value* ast::IdentifierExprAST::codegen() {
