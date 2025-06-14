2a8dffa861425cdbefb0698f24d6e833d942c444
Nana Bingies <nanabingies1@gmail.com>
2025-06-14 14:25:34 +0000

    add codegen

---
+++ include/codegen.hpp
@@ -0,0 +1,11 @@
+#ifndef __CODEGEN_HPP
+#define __CODEGEN_HPP
+
+#include "ast.hpp"
+
+namespace codegen {
+    
+
+};
+
+#endif
---
+++ src/codegen.cpp
@@ -0,0 +1,7 @@
+#include "../include/codegen.hpp"
+
+namespace codegen {
+    
+
+    
+};
